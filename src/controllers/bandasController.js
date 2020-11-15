677a0999ad0c31c892cdffefe9bfa83151537271
elvasthaisa <thaisaelvas@gmail.com>
2020-11-15 00:36:21 -0300

    fixing bug in getById

--- src/controllers/bandasController.js
+++ src/controllers/bandasController.js
@@ -2,35 +2,35 @@ const bandas = require('../models/bandas');
 
 const getAll = (req, res) => {
     console.log(req.url);
-    bandas.find(function(err, bandas) {
+    bandas.find(function(err, banda) {
       if(err) {
         return res.status(424).send({ message : err.message })
       }
-      return res.status(200).send(bandas);
+      return res.status(200).send(banda);
     })
 };
 
-// const getById = (req, res) => {
-//   const id = req.params.id;
+const getById = (req, res) => {
+  const id = req.params.id;
 
-//   bandas.find({ id }, { banda: 1, genero: 1, integrantes: 1,  formacao: 1, _id: 0 }, (err) => {
-//     if (err) {
-//       return res.status(424).send({ message: err.message })
-//     } else if (bandas.length > 0) {
-//       return res.status(200).send(bandas)
-//     } else {
-//       return res.status(404).send("O id não foi encontrado")
-//     }
-//   })
-// }
+  bandas.find({ id }, { banda: 1, genero: 1, integrantes: 1,  formacao: 1, _id: 0 }, (err, banda) => {
+    if (err) {
+      return res.status(424).send({ message: err.message })
+    } else if (bandas.length > 0) {
+      return res.status(200).send(banda)
+    } else {
+      return res.status(404).send("O id não foi encontrado")
+    }
+  })
+}
 
 const getBandaByGenero = (req, res) => {
     console.log(req.url);
     bandas.find({ genero }, { banda: 1, genero: 1, integrantes: 1,  formacao: 1, _id: 0 }, function(err, banda){
       if(err) {
         return res.status(424).send({ message: err.message })
-      } else if (bandas.length > 0) {
-        return res.status(200).send(bandas)
+      } else if (banda.length > 0) {
+        return res.status(200).send(banda)
       } else {
         return res.status(404).send("O gênero não foi encontrado");
       }
@@ -41,9 +41,10 @@ const getBandaByGenero = (req, res) => {
 const postBanda = (req, res) => {
     console.log(req.body);
 
-    let banda = new bandas(req.body);
+    let bandas = new bandas(req.body);
+    console.log(bandas)
 
-    banda.save(function(err) {
+    bandas.save(function(err, banda) {
       if(err) {
         return res.status(424).send({ message: err.message })
       }
@@ -55,7 +56,7 @@ const updateBanda = (req, res) => {
     const id = req.params.id;
     console.log(id);
 
-    bandas.updateMany({ id }, { $set: req.body }, function (err) {
+    bandas.updateMany({ id }, { banda: 1, genero: 1, integrantes: 1,  formacao: 1, _id: 0 }, function (err, bandas) {
       if (err) {
         return res.status(500).send({ message: err.message });
       } else {
@@ -64,23 +65,23 @@ const updateBanda = (req, res) => {
     })
 }
 
-// const deleteBanda = (req, res) => {
-//     const id = req.params.id;
+const deleteBanda = (req, res) => {
+    const id = req.params.id;
 
-//     bandas.deleteMany({ id }, function(err) {
-//       if(err) {
-//         return res.status(424).send({ message: err.message})
-//       } else {
-//         return res.status(200).send(bandas)
-//       }
-//     })
-// };
+    bandas.deleteMany({ id }, function(err, banda) {
+      if(err) {
+        return res.status(424).send({ message: err.message})
+      } else {
+        return res.status(200).send(banda)
+      }
+    })
+};
 
 module.exports = {
     getAll,
-    // getById,
+    getById,
     getBandaByGenero,
     postBanda,
-    // deleteBanda,
+    deleteBanda,
     updateBanda
 }
--- src/routes/bandasRoute.js
+++ src/routes/bandasRoute.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/bandasController.js");
 
 router.post("/", controller.postBanda);
 router.get("/", controller.getAll);
-// router.get("/:id", controller.getById);
+router.get("/:id", controller.getById);
 router.get("/genero/:genero", controller.getBandaByGenero);
 // router.delete("/:id", controller.deleteBanda);
 router.put("/:id", controller.updateBanda);
